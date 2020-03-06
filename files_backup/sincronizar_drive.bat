d5bbc69c248bf9d753b8b912098b7786e702a00b
Maximaube <maximaube@gmail.com>
2020-03-06 12:52:19 -0300

    modificadas paths en archivos .bat de ejemplo

--- files_backup/sincronizar_drive.bat
+++ files_backup/sincronizar_drive.bat
@@ -1,2 +1,2 @@
 @echo off 
-cmd /k "cd /d C:\Users\InfoPer2\Envs\zktime\Scripts & activate & cd /d C:\ZKTime\files_backup & python sinc_zk.py"
+cmd /k "cd /d C:\Users\ruta_de_los_entornos_virtuales\zk\Scripts & activate & cd /d C:\ZKTeco\files_backup & python sinc_zk.py"
